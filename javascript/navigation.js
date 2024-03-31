b2786b6f1a212f6f720e08ed182311498b1c105c
dylankayyem <dyga6971@colorado.edu>
2024-03-30 22:21:39 -0600

    homepage

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -4,9 +4,9 @@ function loadNavigationBar() {
     document.getElementById("nav-placeholder").innerHTML = `
     <div class="topnav" id="myTopnav">
         <a href="index.html" class="active">Home</a>
-        <a href="html/news.html">News</a>
-        <a href="html/contact.html">Contact</a>
-        <a href="html/about.html">About</a>
+        <a href="html/about-me.html">News</a>
+        <a href="html/ui-ux-design.html">Contact</a>
+        <a href="html/software-dev.html">About</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
           <i class="fa fa-bars"></i>
         </a>
