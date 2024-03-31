31af27fc8b807760c09ac4a78a0dc95aaea94e34
dylankayyem <dyga6971@colorado.edu>
2024-03-30 22:52:53 -0600

    navbar

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,16 @@
 /* navigation.js */
 
 function loadNavigationBar() {
+
     document.getElementById("nav-placeholder").innerHTML = `
     <div class="topnav" id="myTopnav">
 
         <a href="/index.html" class="active">Home</a>
+
         <a href="/html/ui.html">UI/UX Projects</a>
+
         <a href="/html/software.html>Software Engineering Projects</a>
+
         <a href="/html/about.html">About Me</a>
 
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
@@ -16,15 +20,18 @@ function loadNavigationBar() {
     </div>
 
     <script>
+
       function myFunction() {
+
         var x = document.getElementById("myTopnav");
+
         if (x.className === "topnav") {
           x.className += " responsive";
         } else {
           x.className = "topnav";
         }
       }
+
     </script>
-    
     `;
 }
